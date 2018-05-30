bf7558bfbcf6be29562ab6c60958e9396077398a
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-05-30 15:32:29 +0530

    Add files via upload

---
+++ src/com/basic/conditional.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class conditional {
+public static void main(String[] args)
+{
+	int count=10;
+	int a = count > 30 ? 25 : 55;
+	System.out.println("value of a is"+a);
+}
+}
