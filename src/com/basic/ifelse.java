18169a781d3fbdb83a61ffccb95c8e3f5b5bdcdd
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-05-30 15:01:10 +0530

    Add files via upload

---
+++ src/com/basic/ifelse.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class ifelse {
+	public static void main(String[] args)
+	{
+		int n=10;
+		if(n==20)
+		{
+			System.out.println("the number is greater than 10");
+		}
+		else
+		{
+			System.out.println("the number is lesser than 10");
+		}
+	}
+
+}
