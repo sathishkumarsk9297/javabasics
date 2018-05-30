80e1da0f29eb7a48694305206e343dea7981ed2e
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-05-30 16:00:33 +0530

    Add files via upload

---
+++ src/com/basic/dowhile.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class dowhile {
+	public static void main(String[] args)
+	{
+		int a=1;
+		do
+		{
+			System.out.println("Do while a = "+ a);
+			a++;
+		}
+			while(a<10);
+}}
