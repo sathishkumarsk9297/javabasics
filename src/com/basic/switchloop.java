b4b65fec50e68129045ec94071b1a944cc2945f0
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-05-30 15:43:39 +0530

    Add files via upload

---
+++ src/com/basic/switchloop.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class switchloop {
+public static void main(String[] args)
+{
+	int count=20;
+	switch (count)
+	{
+	case 10:
+		System.out.println(10);
+		break;
+	case 20:
+		System.out.println(20);
+		break;
+	case 30:
+		System.out.println(30);
+		break;
+	default:
+		System.out.println("default");
+		break;
+	}
+}
+}
